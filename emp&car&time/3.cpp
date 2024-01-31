2257cdae3b093262015e6d5b15b7f9b37cada05f
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-01-31 13:46:28 +0530

    Update 3.cpp

--- emp&car&time/3.cpp
+++ emp&car&time/3.cpp
@@ -10,14 +10,18 @@ class Time
 	   cout<<"enter second:";
 	   cin>>second;
 	   hour = second / 3600;
+	   second=second-(hour*60)
 	   min = second / 60;
-	   second = second % 60;
-	   
-	   cout<<"hh:mm:ss"<<' '<<hour <<':'<<min<< ':'<<second;
+	   second = second -(min*60);
+	   void output()
+	   {  
+	       cout<<"hh:mm:ss"<<' '<<hour <<':'<<min<< ':'<<second;
+	   }
    }
 };
 int main()
 {
 	Time s;
 	s.time();
+	s.output();
 }
