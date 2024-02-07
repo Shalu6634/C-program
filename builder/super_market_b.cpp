b770a46f15f1e7279c7b3804366d75bfa7e34d19
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-02-07 10:28:44 +0530

    super_market_b.cpp

--- builder/super_market_b.cpp
+++ builder/super_market_b.cpp
@@ -15,64 +15,56 @@ using namespace std;
 
 class Market
 {
-	
-		int item_num,quantity[30],price[500];
-		int discount[100];
+	   int item_num,quantity[30],price[500];
+	    int discount[100];
 	    string item_name[50];
 	    string user_id;
-	    static string user;
 	    static int pass;
 	    int password;
-
 	    public:
-		
-	   void setter()
-	   {
+	void setter()
+	{
 
     	    cout << "User_id : "<<endl;
     	    cin >> user_id;
     	    cout << "Password : "<<endl;
     	    cin >> password;
     	    cout<<"....Welcometo super market..."<<endl;
-    	    if(user_id==user&&password==pass)
+    	    if(password==pass)
     	    {
-			
-		       for(int i=1; i<=3; i++)
-	           {
-		         cout << "enter the item_num : ";
-		         cin >> item_num;
-		         cin.ignore();
-			     cout << "enter the item_name : ";
-			     getline(cin,item_name[i]);
+		for(int i=1; i<=3; i++)
+	        {
+		   cout << "enter the item_num : ";
+		   cin >> item_num;
+		   cin.ignore();
+		   cout << "enter the item_name : ";
+	           getline(cin,item_name[i]);
 	
-			     cout << "enter the quantity :";
-			     cin>>quantity[i];
+		   cout << "enter the quantity :";
+		   cin>>quantity[i];
 			     
-			     cout<<"Enter the price :";
-			     cin>>price[i];
-			     cout<<"Enter the discount:";
-			     cin>>discount[i];
-			   
-			     cout<<endl;
-                }
-	    	} 
-	    }
+		   cout<<"Enter the price :";
+		   cin>>price[i];
+		   cout<<"Enter the discount:";
+		   cin>>discount[i];
+		   cout<<endl;
+                 }
+	      } 
+	  }
     	void getter()
-		{
-	    	int i;
-
+	{
+	    int i;
             cout<<endl;
-			cout << "Enter your item_num :"<<endl;
-			cin >> i;
-			cout <<"item_number :"<< i << endl; 
-			cout<<"item_name :" << item_name[i] << endl;
-		    cout <<"Quntity :" <<quantity[i] << endl; 
-		    cout<< "price :" << price[i]<< endl ;
-		    cout<<"Discount :"<<discount[i]<<endl;
+	    cout << "Enter your item_num :"<<endl;
+	    cin >> i;
+	    cout <<"item_number :"<< i << endl; 
+	    cout<<"item_name :" << item_name[i] << endl;
+	     cout <<"Quntity :" <<quantity[i] << endl; 
+	    cout<< "price :" << price[i]<< endl ;
+	    cout<<"Discount :"<<discount[i]<<endl;
         }
 			
 };
-string Market :: user = "dmart_market";
 int Market :: pass = 123456789;
 int main()
 {
