5906b7eac60c1f2e4d13f037bb09c830dec8b3d0
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-02-07 10:42:09 +0530

    Update Railway Reservation.cpp

--- builder/Railway Reservation.cpp
+++ builder/Railway Reservation.cpp
@@ -18,51 +18,46 @@ using namespace std;
 class Railway
 {
 	
-		int train_num,train_time[30];;
-		string train_name[40];
-	    static string destination;
-	    static string source;
-	    public:
-		
+	int train_num,train_time;;
+	string train_name;
+        static string destination;
+	static string source;
+	public:
 	   void setter()
 	   {
-		    for(int i=1; i<=3; i++)
+		for(int i=1; i<=3; i++)
 	        {
 		      cout << "enter the train number : ";
 		      cin >> train_num;
-			  cout << "enter the train time : ";
-			  cin >> train_time[i];
-			  cin.ignore();
-			  cout << "enter the train name :";
-			  getline(cin,train_name[i]);
-			  cout<<endl;
-		
-			}
+		      cout << "enter the train time : ";
+		      cin >> train_time;
+		      cin.ignore();
+		      cout << "enter the train name :";
+		      getline(cin,train_name);
+		      cout<<endl;
+		 }
 			
 	    }
-    	void getter()
-		{
-	    	int i;
-			cout << "Enter and choose your train 1,2,3:"<<endl;
-            cout<<endl;
-			cout << "Enter your train_num :"<<endl;
-			cin >> i;
-			cout <<"train_number :"<< i << endl; 
-			cout<<"Train_time :" << train_time[i] << endl;
-		    cout <<"Train_name :" << train_name[i] << endl; 
-		    cout<< "Source :" << source<< endl ;
-			cout << "Destination :" << destination<< endl ; 
-        }
-			
+    	    void getter()
+	    {
+	    	 int i;
+		cout << "Enter and choose your train :-"<<endl;
+                cout<<endl;
+		cout << "Enter your train_num :"<<endl;
+		cin >> i;
+		cout <<"train_number :"<< i << endl; 
+		cout<<"Train_time :" << train_time<< endl;
+		cout <<"Train_name :" << train_name<< endl; 
+		 cout<< "Source :" << source<< endl ;
+		cout << "Destination :" << destination<< endl ; 
+             }		
 };
 
 string Railway :: source = "Surat Station";
 string Railway :: destination = "Lucknow Station";
-
 int main()
 {
 	Railway r1;
-
-    r1.setter();
+        r1.setter();
 	r1.getter();
 }
