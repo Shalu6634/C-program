a2e10f8190aa8fe621d20520c9ef18d1ee0c54d8
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-02-15 15:24:20 +0530

    Add files via upload

---
+++ Polymorphism/Multiprocesser/default constructor&connect string.cpp
@@ -0,0 +1,30 @@
+//Q.1 Write a Program to create a Message class with a constructor that takes a single string with a default value.
+// Create a private member of the typed string, and in the constructor simply assign the argument string to your internal string.
+//  Create two overloaded member functions called print( ): one that takes no arguments and simply prints the message stored in
+// the variable and one that takes a string argument, which it prints in addition to the internal message.
+#include<iostream>
+using namespace std;
+class Message
+{
+	string msg;
+	public:
+		Message(string msg)
+		{
+			this->msg=msg;
+		}
+		void print()
+		{
+			cout<<msg <<endl;
+		}
+		void print(string message)
+		{
+			cout<<msg << " "<< message<<endl;
+		}
+		
+};
+main()
+{
+	Message m1("Hello");
+	m1.print();
+	m1.print("World");
+}
