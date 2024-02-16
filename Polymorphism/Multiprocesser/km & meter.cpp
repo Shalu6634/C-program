d922aff2451feb130755f23084f0a294fc774e1f
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-02-16 20:42:24 +0530

    km & meter.cpp

--- Polymorphism/Multiprocesser/km & meter.cpp
+++ Polymorphism/Multiprocesser/km & meter.cpp
@@ -9,52 +9,54 @@
 //Km: 8, Meter: 70
 #include<iostream>
 using namespace std;
+
 class Add
 {
-	int km,meter;
-	int km1,meter1,km2,meter2, x=0;
-	public:
-	void set()
-	{
-	   cout<<"Enter km1 : ";
-	   cin>>km1;
-	   cout<<"Enter meter1 : ";
-	   cin>>meter1;
-	   cout<<"Enter km2 : ";
-	   cin>>km2;
-	   cout<<"Enter meter2 : ";
-	   cin>>meter2;
-    }
-
-    void getter()
-    {
+	int km1,meter1,km,meter;
 	
-	   int i;
-	   km=km1+km2;
-	   meter=meter1+meter2;
-	   for(i=1; i<=meter; i++)
-	   {
-		   if(meter>1000)
-		   {
-			  meter-=1000;
-			  x++;
-		   }
-		   
-	   }
-   }
-    void get()
-    {
-    	cout<<"Km -  "<<km + x<<endl;
-	    cout<<"meter -  "<< meter<<endl;
-	}
-	
+	public :
+		void set()
+		{
+			cout << "Enter km :";
+			cin >> km;
+			cout << "Enter meter:";
+			cin >> meter;
+		}
+		void get()
+		{
+			cout << "km :" << km <<" "<< "meter :" << meter << endl;
+		}
+		
+		Add operator+(Add & a2)
+		{
+                     Add d;
+                     int x;
+		    km1= this->km + a2.km;
+		    meter1= this->meter + a2.meter;
+						
+			for(int i=1; i<=meter1; i++)
+			{
+				if(meter1>1000)
+				{
+					x++;
+					meter1 = meter1 - 1000;	
+				}	
+			}
+			
+			      d.km = km1 + x;
+			       d.meter = meter1;
+			
+			       return d;
+		}
 };
-
 int main()
 {
-	 Add a1;
-	 a1.set();
-	 a1.getter();
-     a1.get();
+	Add a1,a2,a3;
 	
+	a1.set();
+	a2.set();
+	
+	a3 = a1 + a2;
+	a3.get();
+	return 0;
 }
