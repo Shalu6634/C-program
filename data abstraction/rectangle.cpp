efb01852ed23a84f61f18f2b14ef066283e71e73
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-02-20 11:14:30 +0530

    rectangle.cpp

--- data abstraction/rectangle.cpp
+++ data abstraction/rectangle.cpp
@@ -3,28 +3,26 @@ using namespace std;
 class Rectangle
 {
 	private:
-		int width,length;
-		public:
-		    Rectangle()
-		    {
-		    	cout<<"Enter width :";
-		    	cin>>width;
-		    	cout<<"Enter length :";
-		    	cin>>length;   
-		    }
-			int area()
-			{
-				cout<<endl;
-				cout<<"area of rectangle"<<":"<<width*length;
-//				return width*length;
-                cout<<endl;
-			}
-			int get()
-			{
-			   cout<<"width :"<<width<<endl<<"height :"<<length;
-		    }
-		
-	
+	int width,length;
+	 public:
+		Rectangle()
+		{
+		    cout<<"Enter width :";
+	            cin>>width;
+		    cout<<"Enter length :";
+		    cin>>length;   
+		}
+                void get()
+		{
+		   cout<<"width :"<<width<<endl<<"height :"<<length;
+                }
+		void area()
+		{
+		    cout<<endl;
+		    cout<<"area of rectangle"<<":"<<width*length;
+                     cout<<endl;
+		}
+			
 };
 main()
 {
